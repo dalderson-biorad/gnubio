f8fff0f246b2bbb8d5d7a64149ef402fdcf1b94b
David <dalderson@dalderson-01.(none)>
2016-04-28 10:23:00 -0400

    Can build pressure bootloaders

--- bootloaders/make_pressure_bootloaders.sh
+++ bootloaders/make_pressure_bootloaders.sh
@@ -3,6 +3,8 @@
 BUILDNAME="twi_atmega328p_SX.hex"
 PRESSNAME="pressure_slaveX.hex"
 PRESSDIR="PressureBootloaders"
+USR_TOOLPATH="/usr/bin"
+USR_ISPPATH-"/usr/bin"
 
 pushd atmega328_twi
 
@@ -14,7 +16,7 @@ fi
 
 for slave in `seq 0 17`
 do
-    make SLAVE_ID=$slave
+    make SLAVE_ID=$slave TOOLPATH=$USR_TOOLPATH ISPPATH=$USR_ISPPATH
     mv obj/${BUILDNAME/X/$slave} ../$PRESSDIR/${PRESSNAME/X/$slave}
 done
 
