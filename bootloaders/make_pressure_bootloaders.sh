03a1a7e0ba52ab65640b46ec9eaba671f46665dc
David <dalderson@dalderson-01.(none)>
2016-05-05 13:55:53 -0400

    Much cleaner make_pressure, now properly aligned

--- bootloaders/make_pressure_bootloaders.sh
+++ bootloaders/make_pressure_bootloaders.sh
@@ -8,16 +8,22 @@ USR_ISPPATH-"/usr/bin"
 
 pushd atmega328_twi
 
-# TODO: Check if dir already exists
+# Create dir if it doesn't exit, empty it if it does
 if [ ! -d "../$PRESSDIR" ]
 then
     mkdir ../$PRESSDIR
+else
+    rm -rf ../$PRESSDIR/*.hex
 fi
+
+
+rm -rf ../$PRESSDIR/*
 
 for slave in `seq 0 17`
 do
     ic2_addr=$(($slave+1)) # i2c_addr is always 1 higher than slave number
-    make SLAVE_ID=$ic2_addr TOOLPATH=$USR_TOOLPATH ISPPATH=$USR_ISPPATH
+    make clean
+    make SLAVE_ID=$ic2_addr TOOLPATH=$USR_TOOLPATH ISPPATH=$USR_ISPPATH OPTIMIZE=1
     mv obj/${BUILDNAME/X/$ic2_addr} ../$PRESSDIR/${PRESSNAME/X/$slave}
 done
 
