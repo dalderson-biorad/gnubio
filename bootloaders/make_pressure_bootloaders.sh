7bfcdeb455ff6ebd8d292d79a8c9baf5044c29f8
David <dalderson@dalderson-01.(none)>
2016-05-05 13:05:47 -0400

    Fixed alignment issue

--- bootloaders/make_pressure_bootloaders.sh
+++ bootloaders/make_pressure_bootloaders.sh
@@ -18,7 +18,7 @@ for slave in `seq 0 17`
 do
     ic2_addr=$(($slave+1)) # i2c_addr is always 1 higher than slave number
     make SLAVE_ID=$ic2_addr TOOLPATH=$USR_TOOLPATH ISPPATH=$USR_ISPPATH
-    mv obj/${BUILDNAME/X/$slave} ../$PRESSDIR/${PRESSNAME/X/$slave}
+    mv obj/${BUILDNAME/X/$ic2_addr} ../$PRESSDIR/${PRESSNAME/X/$slave}
 done
 
 popd
